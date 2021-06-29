ad49c5686b2e9fb2151087792d4ff254558f7329
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-29 19:16:34 -0400

    commit tree bacb8e8da293c650c02b3f12f9fba1166e7034e2

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10);
+
+
+const num3 = 100;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 5, 10, 20);
